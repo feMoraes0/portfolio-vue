cab6cbbc070c17f9dc9d8c35526b6c7a0393f477
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-08-06 22:50:55 -0300

    feat(app): add application header and base style

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="nav-brand" to="/">2021</router-link>
+      <div class="nav-items">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;900&display=swap');
 
-#nav {
-  padding: 30px;
+* {
+  box-sizing: border-box;
+  color: #e9e9ea;
+  font-family: 'Source Sans Pro', sans-serif;
+  margin: 0;
+  padding: 0;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    font-size: 1rem;
+    text-decoration: none;
+  }
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+body {
+  background: #292830;
+  background: radial-gradient(circle, #292830 5%, #0c0c0e 100%);
+  overflow: hidden;
+  height: 100vh;
+}
+
+#app {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  .nav-brand {
+    font-size: 1.1rem;
+    font-weight: 900;
+    letter-spacing: 3px;
+  }
+
+  #nav {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 2.5vh 10vw;
+    width: 100vw;
+
+    .nav-items {
+      a {
+        margin-left: 2vw;
+
+        &:first {
+          margin-left: 0;
+        }
+      }
     }
   }
 }
