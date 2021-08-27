2a513a4ce22b47c8b2a536a5b49f938cbc41f2bd
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-08-26 22:19:07 -0300

    feat(page not found): add image, text and style to 404 error page

--- src/views/errors/PageNotFound.vue
+++ src/views/errors/PageNotFound.vue
@@ -1,11 +1,33 @@
 <template>
   <main>
-    <p>Page not found</p>
+    <img src="@/assets/images/bighead-lost.svg" alt="image">
+    <h3>
+      Probably you are lost and thirsty, click
+      <router-link to='/'>here</router-link>
+      to return to a safe place.
+    </h3>
   </main>
 </template>
 
 <script>
+
 export default {
   name: 'PageNotFound',
 };
 </script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+
+  img {
+    width: 15%;
+    margin-bottom: 50px;
+  }
+}
+</style>
