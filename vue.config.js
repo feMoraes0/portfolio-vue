1c477073c238334d2b74fb43d5ba5ad99a62d322
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-09-19 10:22:10 -0300

    build(webpack): add new webpack property

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        // eslint-disable-next-line no-param-reassign
+        args[0].title = 'Fernando Moraes';
+        return args;
+      });
+  },
   css: {
     loaderOptions: {
       sass: {
