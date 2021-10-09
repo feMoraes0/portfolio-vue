4b1de203b3a7af11b2a30e0b06a73e75443e9bcb
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-09 13:09:58 -0300

    feat(aboutme): adds new component About Me

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h4>
+      <span>FullStack developer</span> coding with <a href="/">JavaScript</a> and TypeScript
+      and their frameworks/libraries like <span>VueJS,
+      ReactJS, React Native, NodeJS </span> and others.
+    </h4>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+
+  h4 {
+    width: 80%;
+    text-align: justify;
+    font-size: 1.6rem;
+    line-height: 140%;
+
+    span {
+      color: $color-black;
+    }
+  }
+}
+</style>
