d574c782d934ae730716a8a4d82e9799a36c6eee
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-17 16:04:29 -0300

    feat(social-card): adds new component to show social media links

---
+++ src/ui/components/SocialCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <Card class="social-card">
+    <a href="/">
+      <img src="@/assets/icons/email.svg" alt="email">
+    </a>
+    <a href="/">
+      <img src="@/assets/icons/linkedin.svg" alt="linkedin">
+    </a>
+    <a href="/">
+      <img src="@/assets/icons/github.svg" alt="github">
+    </a>
+  </Card>
+</template>
+
+<script>
+import Card from '@/ui/elements/Card.vue';
+
+export default {
+  name: 'SocialCard',
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.social-card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+</style>
