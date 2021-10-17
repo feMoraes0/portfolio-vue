c35b3de43f51d0859313a910210b69310ccfeffa
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-17 15:40:46 -0300

    feat(card): add new element to represent the card container

---
+++ src/ui/elements/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  background-color: $white-color;
+  min-height: 300px;
+  border-radius: 16px;
+  padding: 40px;
+}
+</style>
