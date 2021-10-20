2b1eb5810c21b6dc836fd877fa221a136952dd80
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-20 14:48:20 -0300

    feat(custom-label): adds new custom label element

---
+++ src/ui/elements/CustomLabel.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="label">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'CustomLabel',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.label {
+  background: $white-color;
+  font-size: 16px;
+  padding: 5px 25px;
+  border-radius: 5px;
+  text-transform: capitalize;
+}
+</style>
