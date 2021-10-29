c5fbff81777350b7411447abce51936bd33cf530
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-29 15:04:56 -0300

    refactor(theme-card): changes the theme card component to new style with switch toggle

--- src/ui/components/ThemeCard.vue
+++ src/ui/components/ThemeCard.vue
@@ -1,54 +1,33 @@
 <template>
   <Card class="theme small">
-    <div
-      v-for="(theme, index) in themes"
-      :key={index}
-      @click="updateSelected(index)"
-      class="color-selector light-theme"
-      :class="[theme.name, { active: theme.isActive }]"
+    <span>Light</span>
+    <SwitchToggle
+      @changeToggle="updateToggle"
+      :isOn="isLightTheme"
     />
+    <span>Dark</span>
   </Card>
 </template>
 
 <script>
 import Card from '@/ui/elements/Card.vue';
+import SwitchToggle from '@/ui/elements/SwitchToggle.vue';
 
 export default {
   name: 'ThemeCard',
   components: {
     Card,
+    SwitchToggle,
   },
   data: () => ({
-    themes: [
-      {
-        name: 'light-theme',
-        isActive: true,
-      },
-      {
-        name: 'dark-theme',
-        isActive: false,
-      },
-      {
-        name: 'blue-theme',
-        isActive: false,
-      },
-      {
-        name: 'red-theme',
-        isActive: false,
-      },
-    ],
+    isLightTheme: true,
   }),
   methods: {
-    updateSelected(position) {
-      this.themes.forEach((theme, index) => {
-        // eslint-disable-next-line no-param-reassign
-        theme.isActive = false;
-
-        if (index === position) {
-          // eslint-disable-next-line no-param-reassign
-          theme.isActive = true;
-        }
-      });
+    updateToggle() {
+      this.isLightTheme = !this.isLightTheme;
+      const mainContainer = document.querySelector('#main-container');
+      mainContainer.classList = [];
+      mainContainer.classList.add((!this.isLightTheme) ? 'dark-theme' : 'light-theme');
     },
   },
 };
@@ -62,56 +41,8 @@ export default {
   justify-content: space-evenly;
   grid-area: theme-card;
 
-  .color-selector {
-    width: 53px;
-    height: 53px;
-    border-radius: 100%;
-    padding: 4px;
-
-    &::after {
-      content: '';
-      position: absolute;
-      width: 57px;
-      height: 57px;
-      margin-top: -9px;
-      margin-left: -9px;
-      border-radius: 100%;
-      border: 2px solid transparent;
-      background-color: transparent;
-      z-index: +2;
-    }
-
-    &.light-theme {
-      background-image: linear-gradient(to top left, #ffffff, #e9e9e9);
-
-      &.active::after {
-        border: 3px solid black;
-      }
-    }
-
-    &.dark-theme {
-      background-image: linear-gradient(to top left, #000000, #1a1a1a);
-
-       &.active::after {
-        border: 3px solid #ffffff;
-      }
-    }
-
-    &.blue-theme {
-      background-image: linear-gradient(to top left, #002473, #006abc);
-
-       &.active::after {
-        border: 3px solid #ffffff;
-      }
-    }
-
-    &.red-theme {
-      background-image: linear-gradient(to top left, #a70000, #ff7b7b);
-
-       &.active::after {
-        border: 3px solid #ffffff;
-      }
-    }
+  span {
+    font-size: 18px;
   }
 }
 </style>
