00ed995a0afc1c6b3cbf973287c39d0b6608e276
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-30 19:32:55 -0300

    chore(vue-svg-loader): installs svg loader dependency

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,17 @@ module.exports = {
         args[0].title = 'Fernando Moraes';
         return args;
       });
+
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
   },
   css: {
     loaderOptions: {
