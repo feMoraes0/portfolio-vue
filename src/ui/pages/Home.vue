0ab663635fd0ccd41b8cc1c518be680eab97af26
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-12-04 21:45:42 -0300

    Reseting the project to new version

--- src/ui/pages/Home.vue
+++ src/ui/pages/Home.vue
@@ -1,41 +1,11 @@
 <template>
   <main class="page">
-    <section class="grid">
-      <CountCard :counter="1.8" subtitle="years of experience" />
-      <WelcomeCard />
-      <CountCard class="github-card" :counter="210" subtitle="GitHub Stars" />
-
-      <PhotoCard />
-
-      <SocialCard />
-      <AboutCard class="about-card"/>
-
-      <ThemeCard />
-
-      <ProjectsCard />
-    </section>
+    <p>Reseting page</p>
   </main>
 </template>
 
 <script>
-import AboutCard from '@/ui/components/AboutCard.vue';
-import SocialCard from '@/ui/components/SocialCard.vue';
-import CountCard from '@/ui/components/CountCard.vue';
-import WelcomeCard from '@/ui/components/WelcomeCard.vue';
-import PhotoCard from '@/ui/components/PhotoCard.vue';
-import ProjectsCard from '@/ui/components/ProjectsCard.vue';
-import ThemeCard from '../components/ThemeCard.vue';
-
 export default {
   name: 'Home',
-  components: {
-    AboutCard,
-    CountCard,
-    SocialCard,
-    WelcomeCard,
-    PhotoCard,
-    ThemeCard,
-    ProjectsCard,
-  },
 };
 </script>
