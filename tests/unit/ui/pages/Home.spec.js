b753ead99d5f0d2cd94922c8028e31138b2f09b9
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-12-05 13:24:21 -0300

    refactor(home/pagenotfound): removes deprecation warning to validate component name

--- tests/unit/ui/pages/Home.spec.js
+++ tests/unit/ui/pages/Home.spec.js
@@ -1,13 +1,10 @@
 import Home from '@/ui/pages/Home.vue';
-import { shallowMount } from '@vue/test-utils';
 
 describe('Home', () => {
-  const wrapper = shallowMount(Home);
-
   it('Should component name be equal file name', () => {
     // GIVEN
     const filename = 'Home';
     // THEN
-    expect(wrapper.name()).toBe(filename);
+    expect(Home.name).toBe(filename);
   });
 });
--- tests/unit/ui/pages/PageNotFound.spec.js
+++ tests/unit/ui/pages/PageNotFound.spec.js
@@ -1,13 +1,10 @@
 import PageNotFound from '@/ui/pages/PageNotFound.vue';
-import { shallowMount } from '@vue/test-utils';
 
 describe('PageNotFound', () => {
-  const wrapper = shallowMount(PageNotFound);
-
   it('Should component name be equal file name', () => {
     // GIVEN
     const filename = 'PageNotFound';
     // THEN
-    expect(wrapper.name()).toBe(filename);
+    expect(PageNotFound.name).toBe(filename);
   });
 });
